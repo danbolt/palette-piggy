ffb4b8f295ee3e3ee0abd804bf183c4bd7f76d1c
Natasha Miner <nminer@live.ca>
2018-05-11 22:08:44 -0700

    merged Kyle and I's changes

--- main.lua
+++ main.lua
@@ -3,9 +3,6 @@ local bump = require 'lib.bump'
 
 local world = bump.newWorld(50)
 
--- Test Box
-local testBox = {x=100, y=100, w=50, h=50}
-
 local function drawBox(box, r,g,b)
   love.graphics.setColor(r,g,b)
   love.graphics.rectangle("fill", box.x, box.y, box.w, box.h)
@@ -16,10 +13,12 @@ local map = require 'map'
 -- image data
 local imageData = { redSquare = nil }
 
-map.set (2, 4, "red")
-map.set (1, 9, "red")
-map.set (6, 7, "red")
-map.set (3, 6, "red")
+local function initializeMap()
+  map.set (2, 4, "red")
+  map.set (1, 9, "red")
+  map.set (6, 7, "red")
+  map.set (3, 6, "red")
+end
 
 -- Player Stuff
 local player = {x=50, y=50, w=20, h=20, speed=80}
@@ -49,11 +48,21 @@ end
 -- Main LÖVE functions
 function love.load()
   world:add(player, player.x, player.y, player.w, player.h)
-  world:add(testBox, testBox.x, testBox.y, testBox.w, testBox.h)
+  
+  initializeMap()
+
+  for mapx=1,10 do
+    for mapy=1,10 do
+     local tile = map.get(mapx,mapy)
+     if tile == "red" then 
+       local wall = {x= mapx*32, y= mapy*32, w=32, h=32}
+       world:add(wall, wall.x, wall.y, wall.w, wall.h)
+    end
+   end
+ end
   
   imageData.redSquare = love.graphics.newImage('asset/img/square_red.png')
 end
-
 
 function love.update(dt)
   updatePlayer(dt)
@@ -61,13 +70,5 @@ end
 
 function love.draw()
   drawPlayer()
-  drawBox(testBox, 0, 255, 0)
-
-  love.graphics.setColor(255, 255, 255)
-  for x=1,10 do
-    for y=1,10 do
-     local tile = map.get(x,y)
-     if tile == "red" then love.graphics.draw(imageData.redSquare, x * 32, y * 32) end
-   end
- end
+  map.draw(imageData.redSquare)
 end
--- map.lua
+++ map.lua
@@ -11,13 +11,26 @@ for i=1,width do
 end
 
 
-local mymodule = {}
-function mymodule.get (x, y)
+local myModule = {}
+function myModule.get (x, y)
   return map[y][x]
 end
 
-function mymodule.set (x, y, value)
+function myModule.set (x, y, value)
   map[y][x] = value
 end
 
-return mymodule 
+function myModule.draw(tileImage)
+  love.graphics.setColor(255, 255, 255)
+  for mapx=1,width do
+    for mapy=1,height do
+     local tile = myModule.get(mapx,mapy)
+     if tile == "red" then 
+      love.graphics.draw(tileImage, mapx * 32, mapy * 32) 
+    end
+   end
+ end
+end
+
+ 
+return myModule 
