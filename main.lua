1b5607c294432964ee9bd09f5061d97e164df634
Natasha Miner <nminer@live.ca>
2018-05-12 12:01:05 -0700

    working on rendering map based on currentMap and colours

--- main.lua
+++ main.lua
@@ -5,19 +5,40 @@ local mapdata = require 'mapdata'
 
 local world = bump.newWorld(50)
 
-local function drawBox(box, r,g,b)
-  love.graphics.setColor(r,g,b)
-  love.graphics.rectangle("fill", box.x, box.y, box.w, box.h)
-end
-
-local map = require 'map'
+local currentMap = 'red' 
 
 -- image data
 local imageData = { redSquare = nil }
 
-local function initializeMap()
-  --
+local function drawBox(box, r,g,b)
+  love.graphics.setColor(r,g,b)
+  love.graphics.rectangle("fill", box.x, box.y, box.w, box.h)
+  love.graphics.setColor(255, 255, 255)
 end
+ 
+local function getCurrentColour(currentMap)
+  if currentMap == 'red' then
+    return imageData.redSquare
+  elseif currentMap == 'blue' then
+    return imageData.blueSquare
+  elseif currentMap == 'yellow' then
+    return imageData.yellowSquare
+  elseif currentMap == 'green' then
+    return imageData.greenSquare
+  end
+end
+
+local function renderMap(currentMap)
+  for mapx=1,mapdata.getMapWidth(currentMap) do
+    for mapy=1,mapdata.getMapHeight(currentMap) do
+     local tile = mapdata.getTileAt(currentMap, mapx, mapy)
+      if tile == true then
+        currentColour = getCurrentColour(currentMap)
+        love.graphics.draw(currentColour, mapx * 32, mapy * 32) 
+      end
+     end
+   end
+ end
 
 -- Player Stuff
 local player = {x=50, y=50, w=20, h=20, speed=80}
@@ -51,12 +72,10 @@ function love.load(arg)
   if arg[#arg] == "-debug" then require("mobdebug").start() end
   
   world:add(player, player.x, player.y, player.w, player.h)
-  
-  initializeMap()
 
-  for mapx=1,(mapdata.getMapWidth("red")) do
-    for mapy=1,mapdata.getMapHeight('red') do
-     local tile = mapdata.getTileAt('red', mapx, mapy)
+  for mapx=1,(mapdata.getMapWidth(currentMap)) do
+    for mapy=1,mapdata.getMapHeight(currentMap) do
+     local tile = mapdata.getTileAt(currentMap, mapx, mapy)
      if tile == true then 
        local wall = {x= mapx*32, y= mapy*32, w=32, h=32}
        world:add(wall, wall.x, wall.y, wall.w, wall.h)
@@ -65,6 +84,9 @@ function love.load(arg)
  end
   
   imageData.redSquare = love.graphics.newImage('asset/img/square_red.png')
+  imageData.blueSquare = love.graphics.newImage('asset/img/square_blue.png')
+  imageData.greenSquare = love.graphics.newImage('asset/img/square_green.png')
+  imageData.yellowSquare = love.graphics.newImage('asset/img/square_yellow.png')
 end
 
 function love.update(dt)
@@ -73,14 +95,10 @@ end
 
 function love.draw()
   drawPlayer()
-  map.draw(imageData.redSquare)
-   for mapx=1,mapdata.getMapWidth('red') do
-    for mapy=1,mapdata.getMapHeight('red') do
-     local tile = mapdata.getTileAt('red', mapx, mapy)
-      if tile == true then 
-        love.graphics.draw(imageData.redSquare, mapx * 32, mapy * 32) 
-      end
-     end
-   end
+  renderMap(currentMap)
+    
+    
+    
+
    
 end
