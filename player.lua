17f07528d2ede8fa83214373c38c10aec1285a43
Natasha Miner <nminer@live.ca>
2018-05-12 19:15:02 -0700

    added camera shake and refactored player

---
+++ player.lua
@@ -0,0 +1,48 @@
+local function drawBox(box, r,g,b)
+  love.graphics.setColor(r,g,b)
+  love.graphics.rectangle("fill", box.x, box.y, box.w, box.h)
+  love.graphics.setColor(255, 255, 255)
+end
+
+local function initializePlayer()
+  local player = {x=(2*32), y=(14*32), w=20, h=20, speed=80}
+  local speed = player.speed
+  
+  function player.updatePlayer(dt)
+    local dx, dy = 0, 0
+    if love.keyboard.isDown('right') then
+      dx = speed * dt
+    elseif love.keyboard.isDown('left') then
+      dx = -speed * dt
+    end
+    if love.keyboard.isDown('down') then
+      dy = speed * dt
+    elseif love.keyboard.isDown('up') then
+      dy = -speed * dt
+    end
+    return dx, dy
+  end
+    
+  function player.drawPlayer()
+    drawBox(player, 0,255,0)
+  end
+  
+  return player
+end
+
+return initializePlayer()
+
+--[[
+  
+  deltaX, deltaY, collisions, numberofcollisions = world:move(player, player.x + dx, player.y + dy)
+  player.x = deltaX
+  player.y = deltaY 
+  for i=1, numberofcollisions do
+    local collision = collisions[i]
+    if collision.other == endbox then 
+      player.x = 0
+      player.y = 0
+    end
+  end
+end
+--]]
