ecaa25a65b37a9cfb38fd893e75c26d9c7f9b1b9
Daniel Savage <dansvg@gmail.com>
2018-05-13 21:27:14 -0700

    Tweaked title screen

--- scenes/menu.lua
+++ scenes/menu.lua
@@ -12,13 +12,25 @@ function menu:enter()
 title = love.graphics.newImage('asset/img/Title.png')
 end
 
+local function drawDecorationCircles()
+  local radius = 48
+  local yTop = 0
+  local yBottom = love.graphics.getHeight()
+  local offset = ((love.timer.getTime() * 30) % (radius * 2.5))
+  local spacing = radius * 2.5
+  love.graphics.setColor(1, 1, 1, 0.4)
+  for i=-2,(math.floor(love.graphics.getWidth() / radius)) do
+    local horizontalSpot = i * spacing + offset
+    love.graphics.circle("fill", horizontalSpot, yTop, radius)
+    love.graphics.circle("fill", horizontalSpot, yBottom, radius)
+  end
+end
 
-function menu:draw()
+local function calculateTextOffset()
   local magnitudeX = 0
   local magnitudeY = 0
   local textOffsetX = 0
   local textOffsetY = 0
-  
   if math.abs(math.sin(love.timer.getTime() * 1.3)) > 0.8 then
     magnitudeX = math.sin(love.timer.getTime() * 20) * 5 + math.sin(love.timer.getTime() * 26) * 3
     magnitudeY = math.cos(love.timer.getTime() * 24) * 5 + math.cos(love.timer.getTime() * 26) * 3
@@ -26,12 +38,20 @@ function menu:draw()
     textOffsetY = math.sin(love.timer.getTime() * 22) * magnitudeY
   end
   
+  return magnitudeX, magnitudeY
+end
+
+function menu:draw()
+  --drawDecorationCircles()
+  
+  offsetX, offsetY = calculateTextOffset()
+  
   love.graphics.setFont(font)
   love.graphics.setBackgroundColor(1,0.62745098039,0.62745098039)
   love.graphics.setColor(1,1,1,1)
-  love.graphics.draw(title,72,0,0,.8)
+  love.graphics.draw(title,72,64,0,.8)
   love.graphics.setColor(0.51372,0.94509,0.86666)
-  love.graphics.print("Press Enter", math.floor(love.graphics.getWidth() / 2 + textOffsetX), math.floor(love.graphics.getHeight() * 0.65 + textOffsetY), 0, 1, 1, 128)
+  love.graphics.print("Press Enter", math.floor(love.graphics.getWidth() / 2 + offsetX), math.floor(love.graphics.getHeight() * 0.65 + offsetY + 32), 0, 1, 1, 128)
     
 end
 
