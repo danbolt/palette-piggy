76b210629dd7ce7580f87b1dca3122f50f449317
Natasha Miner <nminer@live.ca>
2018-05-13 20:09:38 -0700

    added title logo

---
+++ conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+  t.window.width = 22*32
+  t.window.height = 17*32
+  t.window.title = "Palette Piggy"
+end
--- scenes/menu.lua
+++ scenes/menu.lua
@@ -8,11 +8,15 @@ font = love.graphics.newFont("asset/fonts/Sniglet-Regular.ttf", 75)
 
 endscreen.menu = menu
 
+function menu:enter()
+title = love.graphics.newImage('asset/img/Title.png')
+end
+
+
 function menu:draw()
     love.graphics.setFont(font)
     love.graphics.setBackgroundColor(1,0.62745098039,0.62745098039)
-    love.graphics.setColor(0.57647059, 0.82352941, 0.78039216, 1)
-    love.graphics.print("Palette Piggy", 10, 10)
+    love.graphics.draw(title,72,0,0,.8)
 end
 
 function menu:keyreleased(key, code)
