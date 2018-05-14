2e5589db847477d7f9b7d6db729f86e140912df3
Daniel Savage <dansvg@gmail.com>
2018-05-13 21:42:59 -0700

    Decorate endscreen a little

--- scenes/endscreen.lua
+++ scenes/endscreen.lua
@@ -1,8 +1,24 @@
 local endScreen = {menu = nil}
 
+local function drawDecorationCircles()
+  local radius = 48
+  local yTop = 0
+  local yBottom = love.graphics.getHeight()
+  local offset = ((love.timer.getTime() * 30) % (radius * 2.5))
+  local spacing = radius * 2.5
+  love.graphics.setColor(1, 1, 1, 0.4)
+  for i=-2,(math.floor(love.graphics.getWidth() / radius)) do
+    local horizontalSpot = i * spacing + offset
+    love.graphics.circle("fill", horizontalSpot, yTop, radius)
+    love.graphics.circle("fill", horizontalSpot, yBottom, radius)
+  end
+end
+
 function endScreen:draw()
-    love.graphics.setColor(1,1,1,1)
-    love.graphics.print("Game over. Press Enter to play again.", 10, 10)
+  drawDecorationCircles()
+  
+  love.graphics.setColor(1,1,1,1)
+  love.graphics.printf("You win, kid!", 0, love.graphics.getHeight() / 2 - 25, love.graphics.getWidth(), 'center')
 end
 
 function endScreen:keyreleased(key, code)
