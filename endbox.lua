2ed446e2ed577915db6651d465173ac8c823e5a3
Daniel Alex Savage <danielsavage@yamato.local>
2018-05-13 21:02:23 -0700

    Make endbox all cool

--- endbox.lua
+++ endbox.lua
@@ -2,8 +2,24 @@
 local endBox = {x=20*32, y=2*32, w=32, h=32}
 
 endBox.draw = function()
-  love.graphics.setColor(0, 0, 255)
-  love.graphics.rectangle("fill", endBox.x, endBox.y, endBox.w, endBox.h)
+  love.graphics.push()
+  
+  local move = love.math.newTransform()
+  move:translate(endBox.x + 16, endBox.y + 16)
+  love.graphics.applyTransform(move)
+  
+  local rotate = love.math.newTransform()
+  rotate:rotate(love.timer.getTime() * 8)
+  love.graphics.applyTransform(rotate)
+  
+  local r = math.sin(love.timer.getTime() * 4 ) * 0.2 + 0.8
+  local g = math.sin(love.timer.getTime() * 4 + 2) * 0.2 + 0.8
+  local b = math.sin(love.timer.getTime() * 4 + 4) * 0.2 + 0.8
+  
+  love.graphics.setColor(r, g, b)
+  love.graphics.rectangle("fill", -8, -8, 16, 16)
+  
+  love.graphics.pop()
 end
 
 
