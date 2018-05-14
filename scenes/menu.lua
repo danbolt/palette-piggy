244f8e33989d0a24a1d9b8b0122b2982e995c2f8
Daniel Savage <dansvg@gmail.com>
2018-05-13 21:13:18 -0700

    Make text a little wiggly

--- scenes/menu.lua
+++ scenes/menu.lua
@@ -14,12 +14,24 @@ end
 
 
 function menu:draw()
-    love.graphics.setFont(font)
-    love.graphics.setBackgroundColor(1,0.62745098039,0.62745098039)
-    love.graphics.setColor(1,1,1,1)
-    love.graphics.draw(title,72,0,0,.8)
-    love.graphics.setColor(0.51372,0.94509,0.86666)
-    love.graphics.print("Press Enter", math.floor(love.graphics.getWidth() / 2), math.floor(love.graphics.getHeight() * 0.65), 0, 1, 1, 128)
+  local magnitudeX = 0
+  local magnitudeY = 0
+  local textOffsetX = 0
+  local textOffsetY = 0
+  
+  if math.abs(math.sin(love.timer.getTime() * 1.3)) > 0.8 then
+    magnitudeX = math.sin(love.timer.getTime() * 20) * 5 + math.sin(love.timer.getTime() * 26) * 3
+    magnitudeY = math.cos(love.timer.getTime() * 24) * 5 + math.cos(love.timer.getTime() * 26) * 3
+    textOffsetX = math.cos(love.timer.getTime() * 14) * magnitudeX
+    textOffsetY = math.sin(love.timer.getTime() * 22) * magnitudeY
+  end
+  
+  love.graphics.setFont(font)
+  love.graphics.setBackgroundColor(1,0.62745098039,0.62745098039)
+  love.graphics.setColor(1,1,1,1)
+  love.graphics.draw(title,72,0,0,.8)
+  love.graphics.setColor(0.51372,0.94509,0.86666)
+  love.graphics.print("Press Enter", math.floor(love.graphics.getWidth() / 2 + textOffsetX), math.floor(love.graphics.getHeight() * 0.65 + textOffsetY), 0, 1, 1, 128)
     
 end
 
